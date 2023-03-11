36a4c79c40664dcce1d1c5e01313190c9cf5ff92
Jordo Workstation <jguzak@live.com>
2023-03-10 16:18:05 -0800

    commenting out unused params, adding JF as a I2C output destination

--- lib/mod.lua
+++ lib/mod.lua
@@ -69,7 +69,7 @@ function SendMidiAllNoteAllChannelOff() end
 function SetCvOutputState() end
 function SetMidiSourceDevice() end
 function SetCvAndI2cOutputCallbacks() end
-function SetQuantizedScale() end
+-- function SetQuantizedScale() end
 function SetGateMode() end
 function CrowResetOutputs() end
 
@@ -80,55 +80,50 @@ function InitParams()
 
   -------------------------------------
   -- CV to Midi params
-  params:add_group("cv_to_midi", "CV -> Midi", 8)
+  params:add_group("cv_to_midi", "CV -> Midi/I2C", 6)
   params:add {id = "cv_to_midi_enabled", name = "Enabled", type = "option", options = { "Yes", "No" }, action = function(x) SetCvInputState() end}
-  params:add {id = "cv_to_midi_destination", name = "Destination", type = "option", options = midiDeviceNames, default = 1, action = function(x) SetCvInputState() end}
+  params:add {id = "cv_to_midi_destination", name = "Midi Destination", type = "option", options = midiDeviceNames, default = 1, action = function(x) SetCvInputState() end}
+  params:add {id = "cv_to_i2c_destination", name = "I2C Destination", type = "option", options = { "None", "Just Friends", "W/" }, action = function(x) SetCvInputState() end}
   params:add {id = "cv_to_midi_channel", name = "Midi Channel", type = "number", min = 1, max = 16, default = 1, action = function(x) end}
-  params:add {id = "cv_to_midi_mode", name = "CV Input Mode", type = "option", options = { "Pitch | Gate", "CC" }, action = function(x) SetCvInputState() end}
   params:add_control("cv_to_midi_gate_threshold", "Gate threshold", controlspec.new(-5, 10, "lin", 0.1, 5, "v"))
-  params:add {id = "cv_to_midi_cc_1", name = "CC 1", type = "number", min = 0, max = 127, default = 0, action = function(x) end}
-  params:add {id = "cv_to_midi_cc_2", name = "CC 2", type = "number", min = 0, max = 127, default = 1, action = function(x) end}
   params:add {id = "cv_to_midi_panic", name = "Panic", type = "trigger", action = function(x) SendMidiAllNoteAllChannelOff() end}
 
   -------------------------------------
   -- Midi to CV params
-  params:add_group("midi_to_cv", "Midi -> CV", 15)
+  params:add_group("midi_to_cv", "Midi -> CV", 6)
+  -- params:add_group("midi_to_cv", "Midi -> CV", 15)
   params:add {id = "midi_to_cv_enabled", name = "Enabled", type = "option", options = { "Yes", "No" }, action = function(x) SetCvOutputState() end}
   params:add {id = "midi_to_cv_source", name = "Source", type = "option", options = midiDeviceNames, default = 1, action = function(x) SetCvOutputState() end}
   params:add {id = "midi_to_cv_channel", name = "Channel", type = "number", min = 1, max = 16, default = 1, action = function(x) end}
-  params:add {id = "midi_to_cv_mode", name = "CV Output", type = "option", options = { "1 voice + cc 3 | 4", "2 voices", "4 pitches", "4 gates", "4 CCs", "None" }, action = function(x) SetCvOutputState() end}
-  
-  params:add {id = "midi_to_cv_cc_1", name = "CC 1", type = "number", min = 0, max = 127, default = 0, action = function(x) end}
-  params:add {id = "midi_to_cv_cc_2", name = "CC 2", type = "number", min = 0, max = 127, default = 1, action = function(x) end}
-  params:add {id = "midi_to_cv_cc_3", name = "CC 3", type = "number", min = 0, max = 127, default = 2, action = function(x) end}
-  params:add {id = "midi_to_cv_cc_4", name = "CC 4", type = "number", min = 0, max = 127, default = 3, action = function(x) end}
+  params:add {id = "midi_to_cv_mode", name = "CV Output", type = "option", options = { "None", "1 voice + cc 3 | 4" }, action = function(x) SetCvOutputState() end}
+  -- params:add {id = "midi_to_cv_mode", name = "CV Output", type = "option", options = { "1 voice + cc 3 | 4", "2 voices", "4 pitches", "4 gates", "4 CCs", "None" }, action = function(x) SetCvOutputState() end}
+
+  -- params:add {id = "midi_to_cv_cc_1", name = "CC 1", type = "number", min = 0, max = 127, default = 0, action = function(x) end}
+  -- params:add {id = "midi_to_cv_cc_2", name = "CC 2", type = "number", min = 0, max = 127, default = 1, action = function(x) end}
+  -- params:add {id = "midi_to_cv_cc_3", name = "CC 3", type = "number", min = 0, max = 127, default = 2, action = function(x) end}
+  -- params:add {id = "midi_to_cv_cc_4", name = "CC 4", type = "number", min = 0, max = 127, default = 3, action = function(x) end}
   
   -- TODO: Add envelope class for gate outputs
-  params:add {id = "midi_to_cv_gate_mode", name = "Gate mode", type = "option", options = { "Gate", "Trigger", "ADSR", "ADR" }, action = function(x) SetGateMode() end}
-  params:add {id = "midi_to_cv_attack", name = "Attack", type = "control", controlspec = controlspec.new(0.5, 2000, "lin", 0.001, 1, "ms"), action = function(x) end}
-  params:add {id = "midi_to_cv_decay", name = "Decay", type = "control", controlspec = controlspec.new(0.5, 2000, "lin", 0.001, 200, "ms"), action = function(x) end}
-  params:add {id = "midi_to_cv_sustain", name = "Sustain", type = "control", controlspec = controlspec.new(-60, 0, "lin", 0.01, -20, "dB"), action = function(x) end}
-  params:add {id = "midi_to_cv_release", name = "Release", type = "control", controlspec = controlspec.new(0.5, 2000, "lin", 0.001, 50, "ms"), action = function(x) end}
+  -- params:add {id = "midi_to_cv_gate_mode", name = "Gate mode", type = "option", options = { "Gate", "Trigger", "ADSR", "ADR" }, action = function(x) SetGateMode() end}
+  -- params:add {id = "midi_to_cv_attack", name = "Attack", type = "control", controlspec = controlspec.new(0.5, 2000, "lin", 0.001, 1, "ms"), action = function(x) end}
+  -- params:add {id = "midi_to_cv_decay", name = "Decay", type = "control", controlspec = controlspec.new(0.5, 2000, "lin", 0.001, 200, "ms"), action = function(x) end}
+  -- params:add {id = "midi_to_cv_sustain", name = "Sustain", type = "control", controlspec = controlspec.new(-60, 0, "lin", 0.01, -20, "dB"), action = function(x) end}
+  -- params:add {id = "midi_to_cv_release", name = "Release", type = "control", controlspec = controlspec.new(0.5, 2000, "lin", 0.001, 50, "ms"), action = function(x) end}
 
   params:add {id = "midi_to_cv_oct_offset", name = "Oct Offset", type = "control", controlspec = controlspec.new(-60, 0, "lin", 12, -48, "semitones"), action = function(x) end}
   params:add {id = "midi_to_cv_panic", name = "Panic", type = "trigger", action = function(x) CrowResetOutputs() end}
 
-  -------------------------------------
-  -- I^2C params
-  params:add_group("i^2c", "I^2C", 1)
-  params:add {id = "i2c_device", name = "Device", type = "option", options = { "None", "Just Friends", "W/" }, action = function(x) SetI2CMode() end}
 
   -------------------------------------
   -- Quantize Notes params
-  params:add_group("crovvverter_quantize_notes", "Quantize Notes", 3)
-  params:add {id = "crovvverter_quantizePitch", name = "Quantize Pitch to Scale", type = "option", options = { "No", "Yes" }, action = function(x) end}
-  params:add {id = "crovvverter_root_note", name = "Root Note", type = "option", options = noteNames, action = function(x) SetQuantizedScale() end}
-  params:add {id = "crovvverter_scale", name = "Scale", type = "option", options = { "major", "minor", "dorian" }, action = function(x) SetQuantizedScale() end}
+  -- params:add_group("crovvverter_quantize_notes", "Quantize Notes", 3)
+  -- params:add {id = "crovvverter_quantizePitch", name = "Quantize Pitch to Scale", type = "option", options = { "No", "Yes" }, action = function(x) end}
+  -- params:add {id = "crovvverter_root_note", name = "Root Note", type = "option", options = noteNames, action = function(x) SetQuantizedScale() end}
+  -- params:add {id = "crovvverter_scale", name = "Scale", type = "option", options = { "major", "minor", "dorian" }, action = function(x) SetQuantizedScale() end}
 
   params:show("cv_to_midi")
   params:show("midi_to_cv")
-  params:show("i^2c")
-  params:show("crovvverter_quantize_notes")
+  -- params:show("crovvverter_quantize_notes")
 
   params:bang()
   params:print()
@@ -136,13 +131,13 @@ function InitParams()
 end
 
 -------------------------------------------
--- Global Quantize param action callbacks
-function SetQuantizedScale()
-  local note = params:string("crovvverter_root_note")
-  local scale = params:string("crovvverter_scale")
+-- Quantize param action callbacks
+-- function SetQuantizedScale()
+--   local note = params:string("crovvverter_root_note")
+--   local scale = params:string("crovvverter_scale")
 
-  notesInScale = MusicUtil.generate_scale(params:get("crovvverter_root_note") % 12, scale)
-end
+--   notesInScale = MusicUtil.generate_scale(params:get("crovvverter_root_note") % 12, scale)
+-- end
 
 -------------------------------------------
 -- CV to Midi param action callbacks
@@ -194,17 +189,10 @@ function SetMidiDestinationDevice()
 end
 
 function SetCvInputMode()
-  if params:string("cv_to_midi_mode") == "Pitch | Gate" then
-    crow.input[1].stream = ProcessPitchCv
-    crow.input[2].stream = ProcessGateCv
-    crow.input[1].mode("stream", 0.001, 0.01, "both")
-    crow.input[2].mode("stream", 0.001, 0.01, "both")
-  else
-    crow.input[1].stream = ProcessCc1Cv
-    crow.input[2].stream = ProcessCc2Cv
-    crow.input[1].mode("stream", 0.01, 0.1, "both")
-    crow.input[2].mode("stream", 0.01, 0.1, "both")
-  end
+  crow.input[1].stream = ProcessPitchCv
+  crow.input[2].stream = ProcessGateCv
+  crow.input[1].mode("stream", 0.001, 0.01, "both")
+  crow.input[2].mode("stream", 0.001, 0.01, "both")
 end
 
 function SendMidiAllNoteOff(channel)
@@ -236,10 +224,6 @@ function ProcessPitchCv(voltage)
   end
 
   cvNote = util.round(voltage / 0.0833, 1.0)
-
-  -- if params:string("crovvverter_quantizePitch") == "Yes" then
-  --   cvNote = QuantizeNoteToScale(cvNote)
-  -- end
 end
 
 function ProcessGateCv(voltage)
@@ -251,10 +235,17 @@ function ProcessGateCv(voltage)
       midiDestinationDevice:note_on(cvNote, 100, params:get("cv_to_midi_channel"))
     end
 
+    if params:get("cv_to_i2c_destination") ~= "None" then
+      crow.ii.jf.play_note(cvNote);
+      crow.ii.jf.vtrigger(1, 100);
+    end
+
     lastSentMidiNote = cvNote
     lastDestinationChannel = params:get("cv_to_midi_channel")
   elseif cvGate == true and voltage <= params:get("cv_to_midi_gate_threshold") then
-    print("note "..lastSentMidiNote.." off")
+    if lastSentMidiNote ~= nil then
+      print("note "..lastSentMidiNote.." off")
+    end
     cvGate = false
 
     if midiDestinationDevice ~= nil then
@@ -263,31 +254,6 @@ function ProcessGateCv(voltage)
 
     lastSentMidiNote = nil
     lastDestinationChannel = nil
-  end
-end
-
--- TODO: Make this cleaner, condense into one ProcessCcCv function.
-function ProcessCc1Cv(voltage)
-  if midiDestinationDevice == nil then
-    return
-  end
-
-  local inputVal = util.round(util.linlin(0.0, 10.0, 0, 127, voltage), 1)
-  if lastSentCcMessages[1] ~= inputVal then
-    midiDestinationDevice.cc(params:string("cv_to_midi_cc_1"), inputVal, params:string("cv_to_midi_channel"))
-    lastSentCcMessages[1] = inputVal
-  end
-end
-
-function ProcessCc2Cv(voltage)
-  if midiDestinationDevice == nil then
-    return
-  end
-
-  local inputVal = util.round(util.linlin(0.0, 10.0, 0, 127, voltage), 1)
-  if lastSentCcMessages[2] ~= inputVal then
-    midiDestinationDevice.cc(params:string("cv_to_midi_cc_2"), inputVal, params:string("cv_to_midi_channel"))
-    lastSentCcMessages[2] = inputVal
   end
 end
 
@@ -303,7 +269,7 @@ function SetCvOutputState()
     end
   
     SetCvOutputMode()
-    -- SetI2CMode()
+    SetI2CMode()
     CrowResetOutputs()
 
     -- TODO: figure out how to bind both cv and i2c output callbacks to midi source events
@@ -400,13 +366,13 @@ function ProcessMidiToCv1VoiceAndCc(data)
       RemoveMsgFromMidiNoteQueue(midiSourceActiveNotes, msg)
     end
   elseif msg.type == "cc" then
-    if msg.cc == params:get("midi_to_cv_cc_3") then
-      print("cc: "..msg.cc.." val: "..msg.val)
-      crow.output[3].volts = util.linlin(0, 127, 0, 5, 0.001, msg.val)
-    elseif msg.cc == params:get("midi_to_cv_cc_4") then
-      print("cc: "..msg.cc.." val: "..msg.val)
-      crow.output[4].volts = util.linlin(0, 127, 0, 5, 0.001, msg.val)
-    end
+    -- if msg.cc == params:get("midi_to_cv_cc_3") then
+    --   print("cc: "..msg.cc.." val: "..msg.val)
+    --   crow.output[3].volts = util.linlin(0, 127, 0, 5, 0.001, msg.val)
+    -- elseif msg.cc == params:get("midi_to_cv_cc_4") then
+    --   print("cc: "..msg.cc.." val: "..msg.val)
+    --   crow.output[4].volts = util.linlin(0, 127, 0, 5, 0.001, msg.val)
+    -- end
   end
 end
 
@@ -486,14 +452,12 @@ end
 -------------------------------------------
 -- I2C callbacks
 function SetI2CMode()
-  local i2cMode = params:string("i2c_device")
+  local i2cMode = params:string("cv_to_i2c_destination")
   i2cOutputCallback = nil
   if i2cMode == "Just Friends" then
     crow.ii.pullup(true)
     crow.ii.jf.mode(1)
   elseif i2cMode == "W/" then
-    -- crow.ii.pullup(true)
-    -- crow.ii.
   elseif i2cMode == "None" then
     crow.ii.pullup(false)
   end
@@ -510,5 +474,5 @@ mod.hook.register("script_pre_init", "crovvverter_script_pre_init", function()
   SetCvOutputState()
   SetCvInputState()
 
-  params:set("midi_to_cv_mode", "2 voices")
+  -- params:set("midi_to_cv_mode", "2 voices")
 end)
